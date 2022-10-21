946d14ac0f27a54acdaaee360b5210e6f5502533
maurice-cubicit <maurice@cubicit.co.za>
2022-10-21 23:41:26 +0200

    Icluded contact form

---
+++ components/forms/Contact.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
+
+export default function Contact() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [loading,setLoading] = useState(false);
+  
+  const onSubmit = async(data) => {
+    const {firstName,email,phone,subject,message} = data;
+    if(firstName){
+        setLoading(!loading)
+        console.log(data)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000);
+    }else{
+        setLoading(false)
+    }
+    await axios.post("http://localhost:5000/subform",{data:data})
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  }; 
+
+  return (
+    <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className='w-full'>
+        <div>Name</div>
+        <input className='w-full rounded p-2 my-2 border border-gray-500' type="text" {...register("firstName", { required: true, maxLength: 20 })} />
+        {errors.firstName && <span className='text-red-600 text-sm'>This field is required</span>}
+        <div>Email</div>
+        <input className='w-full rounded p-2 my-2 border border-gray-500' type="email" {...register("email", { required: true, maxLength: 20 })}/>
+        {errors.email && <span className='text-red-600 text-sm'>This field is required</span>}
+        <div>Phone</div>
+        <input className='w-full rounded p-2 my-2 border border-gray-500' type="tel" {...register("phone", { required: true, maxLength: 20 })}/>
+        {errors.phone && <span className='text-red-600 text-sm'>This field is required</span>}
+        <div>Subject</div>
+        <select className='w-full rounded p-2 my-2 border border-gray-500' {...register("subject", { required: true, maxLength: 20 })}>
+            <option>Select</option>
+            <option value="information">Information</option>
+            <option value="complaint">Complaint</option>
+            <option value="booking">Booking</option>
+            <option value="help">Help</option>
+        </select>
+        {errors.subject && <span className='text-red-600'>This field is required</span>}
+        <div>Message</div>
+        <textarea className='w-full rounded p-2 h-36 my-2 border border-gray-500' {...register("message")}/>
+        {errors.message && <span className='text-red-600 text-sm'>This field is required</span>}
+        <button className="bg-black text-white w-full rounded py-2 hover:bg-gray-800 cursor-pointer hover:border-r-4 hover:border-b-4 hover:border-pink-500 text-md hover:bg-green-600">
+        {loading ? <div className='flex w-full'><div className="loader2 m-auto"></div></div> : 'Subscribe'}
+        </button>
+    </form>
+  )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ContactForm from '../components/forms/Contact'
 import Hero from '../components/general/Hero'
 
 export default function Home() {
@@ -10,6 +11,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero height='h-[35vh]' />
+
+      <div className='flex flex-wrap w-full justify-center'>
+        <div className='flex flex-wrap w-11/12 md:w-9/12 py-10'>
+          <div className='w-full md:w-1/2'>
+            <ContactForm />
+          </div>
+          <div className='w-full md:w-1/2 md:pl-10 mt-10 md:mt-0'>
+            <div className='text-xl font-semibold'>
+              Address
+            </div>
+            <div className='mt-5'>
+              13 Myrtle Road<br></br> Rondebosch<br></br> Western Cape<br></br> Cape Town<br></br> 7700
+            </div>
+            <div className='text-xl font-semibold mt-10'>
+              Contact
+            </div>
+            <div className='mt-5'>
+              +27 67 642 0911 <br></br>+27 694 387 5111
+            </div>
+            <div className='w-full bg-green-100 h-52 mt-10'>
+
+            </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
