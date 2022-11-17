58d95273a21a66f96533fe196f89fa3ff92d4b8a
maurice-cubicit <maurice@cubicit.co.za>
2022-11-17 20:23:31 +0200

    fix: sub form

--- pages/api/subscribers.js
+++ pages/api/subscribers.js
@@ -1,10 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import nodemailer from 'nodemailer';
 
 export default async function handel (req, res){
   const  email  = req.body;
-
-  console.log(email)
 
   let transporter = nodemailer.createTransport({
     host: 'mail.petconcierge.co.za',
@@ -20,16 +17,20 @@ export default async function handel (req, res){
   });
 
   try {
-    let email = await transporter.sendMail({
+    
+    let emailResult = await transporter.sendMail({
       from: 'info@petconcierge.co.za',
       to: 'info@petconcierge.co.za' ,
-      subject: 'Subscriber from website with following Email',
+      subject: 'Contact form information',
       html:`<p>${email}</p>`
+    
     });
 
-    console.log("Message Sent", email.messageId)
-    res.status(200).json(email.messageId)
-  } catch (error) {
+    console.log("Message Sent", emailResult.messageId)
+    res.status(200).json(emailResult.messageId)
+  
+} catch (error) {
     res.status(400).json(error)
   }
+
 }
